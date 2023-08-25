6f6ab467ae8f38d26cdf9bdf1af938c05816b5f5
brojor <bronislav.jordan@gmail.com>
2023-08-25 18:38:08 +0200

    refactor: abstract screencard to component

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
 			<h1>Hi, <br> I'm <span>Brojor</span>, <br> web developer </h1>
 			<p>I like to create amazing things using web technologies</p>
 		</div>
-		<div class="screen">
-			<div class="screen-image" />
-			<div class="screen-overlay"></div>
-			<div class="screen-content"></div>
-		</div>
+    <ScreenCard />
 	</section>
 	<section>
 		<h2>
@@ -21,6 +17,7 @@
 
 <script setup lang="ts">
 import IconCards from './components/IconCards.vue';
+import ScreenCard from './components/ScreenCard.vue';
 
 
 </script>
@@ -68,94 +65,5 @@ h1 span{
 
 
 
-.screen {
-	width: 200px;
-	border: 2px solid rgb(var(--primary-rgb)/80%);
-	aspect-ratio: 10/16;
-	border-radius: 10px;
-	background-color: rgb(var(--primary-rgb)/15%);
-	overflow: hidden;
-	position: relative;
-	display: flex;
-}
 
-.screen>.screen-image {
-	background-image: url('@/assets/photo.png');
-	background-repeat: no-repeat;
-	height: 100%;
-	width: 100%;
-	position: absolute;
-	z-index: 1;
-	left: 0px;
-	top: 0px;
-	background-size: 150%;
-	background-position: 60% 100%;
-	filter: sepia(1) hue-rotate(150deg);
-	opacity: 0.8;
-}
-
-@keyframes pan-overlay {
-	from {
-		background-position: 0% 0%;
-	}
-
-	to {
-		background-position: 0% -100%;
-	}
-}
-
-.screen-overlay {
-	background: linear-gradient(rgb(var(--primary-rgb) / 0.15),
-			rgb(var(--primary-rgb) / 0.15) 3px,
-			transparent 3px,
-			transparent 6px);
-	background-size: 100% 6px;
-	height: 100%;
-	width: 100%;
-	animation: pan-overlay 22s infinite linear;
-	position: absolute;
-	z-index: 2;
-	left: 0px;
-	top: 0px;
-}
-
-.screen>.screen-content {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: flex-end;
-	flex-grow: 1;
-	gap: 4rem;
-	position: relative;
-	z-index: 3;
-	margin: 10px;
-	padding-bottom: 6rem;
-	border: 1px solid rgb(var(--primary-rgb) / 70%);
-	border-radius: 6px;
-}
-
-.screen:after,
-.screen:before {
-	content: "";
-	height: 3px;
-	position: absolute;
-	z-index: 4;
-	left: 50%;
-	translate: -50% 0%;
-	background-color: white;
-}
-
-.screen:before {
-	width: 15%;
-	top: 0rem;
-	border-bottom-left-radius: 1rem;
-	border-bottom-right-radius: 1rem;
-}
-
-.screen:after {
-	width: 25%;
-	bottom: 0rem;
-	border-top-left-radius: 1rem;
-	border-top-right-radius: 1rem;
-}
 </style>
